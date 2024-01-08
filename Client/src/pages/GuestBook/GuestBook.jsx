639e03acc273eef74ea16f7ade4bb3d3fbb2561c
kcmdusgml3@naver.com <gktmzl6026!>
2024-01-08 10:45:26 +0900

    [FE]🎨 Design: 버튼 크기 수정 및 베리언트 추가 #7

--- Client/src/pages/GuestBook/GuestBook.jsx
+++ Client/src/pages/GuestBook/GuestBook.jsx
@@ -1,5 +1,27 @@
+import Button from "../../components/Button/Button.jsx";
 function GuestBook() {
-  return "방명록";
+  return (
+    <>
+      <div>
+        <Button size="sm" variant="lightbtn">
+          smbtn
+        </Button>
+      </div>
+      <div>
+        <Button size="md" variant="graybtn">
+          mdbtn
+        </Button>
+      </div>
+      <div>
+        <Button size="lg" variant="subbtn">
+          lgbtn
+        </Button>
+      </div>
+      <div>
+        <Button>bsbtn</Button>
+      </div>
+    </>
+  );
 }
 
 export default GuestBook;

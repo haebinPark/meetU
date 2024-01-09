4e94a7a3a41176162beba9058d44ae1ae710c574
kimInDa <pinktail2327@naver.com>
2024-01-09 18:03:05 +0900

    [FE]✨Feat : 페이지 소개말 컴포넌트 구현

---
+++ Client/src/components/Common/PageDescription.jsx
@@ -0,0 +1,9 @@
+import { styled } from "styled-components";
+
+const PageDescription = styled.span`
+  color: var(--font-gray);
+  display: inline-block;
+  text-align: center;
+`;
+
+export default PageDescription;

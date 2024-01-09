936ed656a80de09a0400be4627726d9429e7045f
kimInDa <pinktail2327@naver.com>
2024-01-09 18:04:31 +0900

    Revert "[FE]✨Feat : 페이지 소개말 컴포넌트 구현"
    This reverts commit 4e94a7a3a41176162beba9058d44ae1ae710c574.

--- Client/src/components/Common/PageDescription.jsx
+++
@@ -1,9 +0,0 @@
-import { styled } from "styled-components";
-
-const PageDescription = styled.span`
-  color: var(--font-gray);
-  display: inline-block;
-  text-align: center;
-`;
-
-export default PageDescription;

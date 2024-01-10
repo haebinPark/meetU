5da047c64df073ac39c206f4375528c1f76d21d4
kcmdusgml3@naver.com <gktmzl6026!>
2024-01-10 15:19:04 +0900

    [FE]✨ feat : 방명록페이지 마크업 및 CSS #9

---
+++ Client/src/components/GestBook/GB_Comment.jsx
@@ -0,0 +1,47 @@
+import { styled } from "styled-components";
+const CommentEntry = styled.li`
+  /* 방명록 항목에 대한 스타일 */
+  border-bottom: 2px solid Var(--box-gray);
+  width: 90%;
+  height: 4vh;
+  padding: 3px;
+  margin-left: 30px;
+  margin-right: 30px;
+  margin-top: 3px;
+  display: flex;
+  justify-items: center;
+  align-items: center;
+  flex-direction: column;
+  flex-direction: row;
+`;
+
+const CommentNameStyle = styled.span`
+  /* 방명록 이름에 대한 스타일 */
+  font-weight: 700;
+  font-size: 1rem;
+  flex: 0.5;
+`;
+
+const CommentStyle = styled.span`
+  /* 방명록 내용에 대한 스타일 */
+  flex: 1;
+  font-size: 1rem;
+  padding-left: 1rem;
+`;
+const CommentDateStyle = styled.span`
+  /* 방명록 항목 내용에 대한 스타일 */
+  font-size: 0.8rem;
+  color: var(--font-gray);
+  text-align: right;
+  flex: 1;
+`;
+const Gestcomment = () => {
+  return (
+    <CommentEntry>
+      <CommentNameStyle> 오우예씨몬 </CommentNameStyle>
+      <CommentStyle>마치고 피방ㄱ?</CommentStyle>
+      <CommentDateStyle>2024.03.04.10:36</CommentDateStyle>
+    </CommentEntry>
+  );
+};
+export default Gestcomment;
---
+++ Client/src/components/GestBook/GB_CommentBox.jsx
@@ -0,0 +1,29 @@
+import { styled } from "styled-components";
+import Gestcomment from "./GB_Comment.jsx";
+const GBViewStyle = styled.div`
+  margin-top: 10px;
+  padding: 10px;
+  border: 1px solid var(--font-lightgray);
+  width: 100%;
+  height: auto;
+`;
+
+function GestBookView() {
+  return (
+    <GBViewStyle>
+      <Gestcomment />
+      <Gestcomment />
+      <Gestcomment />
+      <Gestcomment />
+      <Gestcomment />
+      <Gestcomment />
+      <Gestcomment />
+      <Gestcomment />
+      <Gestcomment />
+      <Gestcomment />
+      <Gestcomment />
+    </GBViewStyle>
+  );
+}
+
+export default GestBookView;

80eda42419cbca1d9e181866fd9be1263e2e4858
kim_junyeon <martemper@naver.com>
2024-01-15 09:20:46 +0900

    :sparkles: Feat:배경색 조회,수정 구현 #59

--- Server/be-springboot-starter-main/src/main/java/com/example/codestates/user/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.codestates.user.repository;
-
-import com.example.codestates.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> { //기본 CRUD 메서드 포함됨
-}

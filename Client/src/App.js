42af71f49535fa1e6be4e47db80a0369d2c2b0d2
kimInDa <pinktail2327@naver.com>
2024-01-17 17:15:27 +0900

    [FE]🚚Rename : 토스트 알림 컨테이너 이동  #77

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
 import PageLayout from "./layout/PageLayout.jsx";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <>
       <PageLayout>
         <Outlet />
+        {/* 토스트 알림 */}
+        <ToastContainer />
       </PageLayout>
     </>
   );

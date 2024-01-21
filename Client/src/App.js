15143261cd6d6546fe1514140de0fd61e2a2f850
kimInDa <pinktail2327@naver.com>
2024-01-22 01:19:00 +0900

    [FE]✏️Fix : 페이지 전환시 로딩스피너 삽입 #88

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,10 +3,11 @@ import router from "./routes";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Suspense } from "react";
+import Spinner from "./components/Common/Spinner.jsx";
 
 function App() {
   return (
-    <Suspense fallback="...loading">
+    <Suspense fallback={<Spinner />}>
       <RouterProvider router={router} />
       {/* 토스트 알림 */}
       <ToastContainer />
